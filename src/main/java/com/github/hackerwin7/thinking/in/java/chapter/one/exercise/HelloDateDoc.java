627505fe2a8ef214dffd5a48b6751f8fa129d501
hackerwin7 <1186897618@qq.com>
2016-04-08 09:58:53 +0800

    upload

---
+++ src/main/java/com/github/hackerwin7/thinking/in/java/chapter/one/exercise/HelloDateDoc.java
@@ -0,0 +1,19 @@
+package com.github.hackerwin7.thinking.in.java.chapter.one.exercise;
+
+/**
+ * The HelloWorld program implements an application that
+ * simply displays "Hello World!" to the standard output.
+ *
+ * <h1> Hello, World! </h1>
+ * <p> segment </p>
+ *
+ * @author zara Ali
+ * @version 1.0
+ * @since 20160407
+ */
+public class HelloDateDoc {
+    public static void main(String[] args) throws Exception {
+        /* prints hello world (or Date) */
+        System.out.println("Hello World!");
+    }
+}
